0c10433e87edc298294a3fab6ca7a3c3bcd3bdcb
viller_m <mickael.villers@epitech.eu>
2015-08-19 16:38:30 +0200

    correction responsive

--- themes/MyTheme/js/autoload/main.js
+++ themes/MyTheme/js/autoload/main.js
@@ -2,9 +2,16 @@
  * Created by viller_m on 19/08/2015.
  */
 $(function() {
-    $( document ).ready(function() {
+    $(document).ready(function() {
         $('body').height($(document).height());
         $('body').width($(document).width());
         $('body').particleground();
+    });
+
+    $(window).resize(function() {
+        $('body').height($(window).height());
+        $('body').width($(window).width());
+        $('body').particleground();
+        console.log('rezise');
     });
 });
